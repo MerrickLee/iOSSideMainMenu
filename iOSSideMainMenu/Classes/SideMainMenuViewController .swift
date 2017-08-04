94917cdbb946e8f7f359fa6dde71159cc53b7f9e
Merrick Lee <merricklee@me.com>
2017-08-04 15:59:54 -0400

    Slides back and Added a Bounce

--- iOSSideMainMenu/Classes/SideMainMenuViewController .swift
+++ iOSSideMainMenu/Classes/SideMainMenuViewController .swift
@@ -3,42 +3,78 @@
 //  Pods
 //
 //  Created by Juan Morillo on 20/5/17.
+//  Updates by Merrick Lee on 4/8/17.
 //
 //
 
 import Foundation
 import UIKit
 
+
+//The width of your menu
+var startingWidthPosition:CGFloat = -207
+
+//Allow Bouncing = TRUE
+//Remove Boucing = FALSE
+var bouncingON = true
+
+
+
 public class SideMainMenuViewController : UIViewController {
-  
-  //IBOulets
-  @IBOutlet var leftContraings: NSLayoutConstraint!
-  @IBOutlet var leftMainMenu: UIView!
-  
-  //vars
-  var mainMenuActive = false
-  
-  override public func viewDidLoad() {
-    super.viewDidLoad()
     
-    //Properties of Views
-    leftMainMenu.layer.shadowOpacity = 1
-    leftMainMenu.layer.shadowRadius = 6
+    //IBOulets
+    @IBOutlet var leftContraings: NSLayoutConstraint!
+    @IBOutlet var leftMainMenu: UIView!
     
-  }
-  
-  //Actions
-  @IBAction func openMainMenu(_ sender: Any) {
-    if (mainMenuActive) {
-      leftContraings.constant = -207
-    } else {
-      leftContraings.constant = 0
-      UIView.animate(withDuration: 0.3, animations: {
-        self.view.layoutIfNeeded()
-      })
-      
+    //vars
+    var mainMenuActive = false
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Properties of Views
+        leftMainMenu.layer.shadowOpacity = 1
+        leftMainMenu.layer.shadowRadius = 6
+        
     }
-    mainMenuActive = !mainMenuActive
-  }
-  
+    
+    //Actions
+    @IBAction func openMainMenu(_ sender: Any) {
+        if (mainMenuActive) {
+            leftContraings.constant = startingWidthPosition
+            UIView.animate(withDuration: 0.25, animations: {
+                self.view.layoutIfNeeded()
+            })
+            
+            if(bouncingON){
+                
+                leftContraings.constant = leftContraings.constant + 15
+                UIView.animate(withDuration: 0.55, delay: 0.1, animations: {
+                    self.view.layoutIfNeeded()
+                })
+                leftContraings.constant  = startingWidthPosition
+                UIView.animate(withDuration: 0.25, delay: 0.5, animations: {
+                    self.view.layoutIfNeeded()
+                })
+            }
+        } else {
+            leftContraings.constant = 0
+            UIView.animate(withDuration: 0.25, animations: {
+                self.view.layoutIfNeeded()
+            })
+            if(bouncingON){
+                leftContraings.constant = leftContraings.constant - 40
+                UIView.animate(withDuration: 0.55, animations: {
+                    self.view.layoutIfNeeded()
+                })
+                leftContraings.constant = 0
+                UIView.animate(withDuration: 0.25, animations: {
+                    self.view.layoutIfNeeded()
+                })
+            }
+            
+        }
+        mainMenuActive = !mainMenuActive
+    }
+    
 }
